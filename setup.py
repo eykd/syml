efcd9a34b44f69a632be9d5acc49b0d8a4a5770b
deyk <deyk@crossway.org>
2017-03-05 13:35:18 -0800

    Include README in long_description

--- setup.py
+++ setup.py
@@ -3,28 +3,31 @@ from setuptools import setup, find_packages
 import versioneer
 
 
-setup(
-    name = 'syml',
-    version=versioneer.get_version(),
-    author = "David Eyk",
-    author_email = "david.eyk@gmail.com",
-    cmdclass=versioneer.get_cmdclass(),
-    description = (
-        "SYML (Simple YAML-like Markup Language) is a simple markup "
-        "language with similar structure to YAML, but without all the gewgaws "
-        "and folderol."
-    ),
-    packages = find_packages(),
-    install_requires = [
-        'attrs<17',
-        'parsimonious<0.8',
-    ],
-    keywords = ['yaml', 'markup', 'syml'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup',
-    ],
-)
+with open('README.rst') as README:
+    setup(
+        name = 'syml',
+        version=versioneer.get_version(),
+        author = "David Eyk",
+        author_email = "david.eyk@gmail.com",
+        cmdclass=versioneer.get_cmdclass(),
+        description = (
+            "SYML (Simple YAML-like Markup Language) is a simple markup "
+            "language with similar structure to YAML, but without all the gewgaws "
+            "and folderol."
+        ),
+        long_description = README.read(),
+        url = 'https://github.com/eykd/syml',
+        packages = find_packages(),
+        install_requires = [
+            'attrs<17',
+            'parsimonious<0.8',
+        ],
+        keywords = ['yaml', 'markup', 'syml'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Text Processing :: Markup',
+        ],
+    )
