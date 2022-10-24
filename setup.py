f11315a267e22226f832cff0285ba9b58db17bf7
eykd <david@eykd.net>
2022-10-23 18:25:57 -0700

    Move to src/ layout

--- setup.py
+++ setup.py
@@ -1,34 +1,9 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 import versioneer
 
-
-with open('README.rst') as README:
-    setup(
-        name = 'syml',
-        version=versioneer.get_version(),
-        author = "David Eyk",
-        author_email = "david.eyk@gmail.com",
-        cmdclass=versioneer.get_cmdclass(),
-        description = (
-            "SYML (Simple YAML-like Markup Language) is a simple markup "
-            "language with similar structure to YAML, but without all the gewgaws "
-            "and folderol."
-        ),
-        long_description = README.read(),
-        url = 'https://github.com/eykd/syml',
-        license = 'MIT',
-        packages = find_packages(),
-        install_requires = [
-            'attrs',
-            'parsimonious',
-        ],
-        keywords = ['yaml', 'markup', 'syml'],
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Programming Language :: Python :: 3.5',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'Topic :: Text Processing :: Markup',
-        ],
-    )
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
